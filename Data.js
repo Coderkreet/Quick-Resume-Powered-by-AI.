1d600e05e05e80d9ebf481659bbf641529ff99f2
Coderkreet <krunalbende2@gmail.com>
2024-09-28 10:50:23 +0530

    Shortcut

---
+++ Data.js
@@ -0,0 +1,87 @@
+ let Data = [
+    {
+      "id": 1,
+      "name": "Eiffel Tower",
+      "price": {
+        "adult": 25,
+        "child": 12,
+        "currency": "EUR"
+      },
+      "description": "The Eiffel Tower is an iconic landmark in Paris, France, offering breathtaking views of the city."
+    },
+    {
+      "id": 2,
+      "name": "Great Wall of China",
+      "price": {
+        "adult": 40,
+        "child": 20,
+        "currency": "CNY"
+      },
+      "description": "A historic fortification stretching thousands of miles, the Great Wall of China is one of the Seven Wonders of the World."
+    },
+    {
+      "id": 3,
+      "name": "Statue of Liberty",
+      "price": {
+        "adult": 23,
+        "child": 10,
+        "currency": "USD"
+      },
+      "description": "The Statue of Liberty is a symbol of freedom and democracy, located on Liberty Island in New York City."
+    },
+    {
+      "id": 4,
+      "name": "Taj Mahal",
+      "price": {
+        "adult": 15,
+        "child": 5,
+        "currency": "INR"
+      },
+      "description": "The Taj Mahal is a magnificent marble mausoleum in Agra, India, considered one of the most beautiful buildings in the world."
+    },
+    {
+      "id": 5,
+      "name": "Sydney Opera House",
+      "price": {
+        "adult": 30,
+        "child": 15,
+        "currency": "AUD"
+      },
+      "description": "An architectural masterpiece, the Sydney Opera House is a world-famous performing arts center located in Sydney, Australia."
+    },
+    {
+      "id": 6,
+      "name": "Colosseum",
+      "price": {
+        "adult": 18,
+        "child": 8,
+        "currency": "EUR"
+      },
+      "description": "The Colosseum is an ancient Roman amphitheater in Rome, Italy, known for its grandeur and history of gladiatorial combats."
+    },
+    {
+      "id": 7,
+      "name": "Machu Picchu",
+      "price": {
+        "adult": 50,
+        "child": 25,
+        "currency": "PEN"
+      },
+      "description": "Machu Picchu is a 15th-century Incan citadel located in the Andes Mountains of Peru, renowned for its breathtaking views and historical significance."
+    },
+    {
+      "id": 8,
+      "name": "Mount Fuji",
+      "price": {
+        "adult": 0,
+        "child": 0,
+        "currency": "JPY"
+      },
+      "description": "Mount Fuji is Japan’s highest mountain, revered as a sacred symbol and a popular destination for hikers and sightseers."
+    }
+  ]
+  
+
+  export default Data;
+
+// JavaScript Code:
--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,11 @@
-import { useState, useEffect } from 'react';
-import './App.css';
-import Services from './Components/Services';
-import ThreeModel from './Components/ThreeModel';
-
-function App() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    // Set a timer to hide the spinner after 2 seconds
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 3000);
-
-    // Cleanup timer when the component is unmounted
-    return () => clearTimeout(timer);
-  }, []);
-
+import React from 'react'
+import './App.css'
+const App = () => {
   return (
     <div>
-      {/* Show ThreeModel spinner for 2 seconds, then show Services */}
-      {loading ? <ThreeModel /> : <Services />}
+      hello
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
